197677c1b04ffbd32fabfd66904f4e294003ad08
priyagosain <pr932087@dal.ca>
2017-06-14 14:14:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/priyagosain/smartdrumsticks/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.priyagosain.smartdrumsticks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+   TextView hitText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        }
+}
+
+
